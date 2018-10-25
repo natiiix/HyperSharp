c4907a14a04fd06bf6406754388e79771c67ebf4
natiiix <natiwastaken@gmail.com>
2018-10-25 21:12:40 +0200

    Initialize new C# .NET project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HyperSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
